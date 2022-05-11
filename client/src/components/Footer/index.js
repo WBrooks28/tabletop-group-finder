9db4f4ee8c6cfbb230218fc66e522788eb00450a
Cindy <cindydunham4@gmail.com>
2022-05-11 14:57:32 -0400

    Footer location

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,12 +3,17 @@ import React from 'react'
 export default function Footer() {
     const footer = "Footer"
     return (
-        <>
+        <div style={{
+            position: "relative",
+            left: 0,
+            bottom: 0,
+            right: 0
+        }}>
             <ul className="flex-row">
                 <li className="mx-1">
-                    <span>&copy; 2022 Table-Top Game Finder; Tyler Davis, Cindy Dunham, William Brooks, Karen Richardson</span>
+                    <span className="text-dark">&copy; 2022 Table-Top Game Finder; Tyler Davis, Cindy Dunham, William Brooks, Karen Richardson</span>
                 </li>
             </ul>
-        </>
+        </div>
     )
 }
